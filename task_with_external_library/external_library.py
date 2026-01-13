4c8ed18cd473d0d148c8eb9cd1265bdd2751891f
Kasia <kasiakos81@gmail.com>
2026-01-13 17:50:40 +0100

    weather info task

---
+++ task_with_external_library/external_library.py
@@ -0,0 +1,67 @@
+# Create a program, which will scheck if it is going to rain on a particular day
+#
+# Program asks for a weather check in a given place and on given date. Use the below given API. It should work like this:
+#
+# - program asks for which date the weather shall be checked. Data has to be in format of YYYY-mm-dd, i.e. 2022-07-01.
+# - if the date is not provided the app considers the next day as a target date
+# - the app will make a call to the API to check the weather
+# - there are three options to be received as a response about if it is going to rain:
+#  1 - it will rain
+#  2 - It won't rain
+#  3 - no f*** idea
+#  - the response should be stored in a file. If the searched date is already present in a file return the response from the file
+#
+# API URL: https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}
+#
+# the following parameters need to be replaced in the URL: latitude, longitude and searched_date
+
+import requests
+from file_handler_rain import FileHandler
+#
+from geopy.geocoders import Nominatim
+file_handler = FileHandler("data.json")
+
+
+city = input("Provide the name of a city: ")
+searched_date = input("Privide date (YYYY-MM-DD): ")
+
+geolocator = Nominatim(user_agent="some_app")
+location = geolocator.geocode(city)
+
+print(location)
+latitude = location.latitude
+longitude = location.longitude
+
+print(file_handler.check_if_date_exists(city, searched_date))
+
+def get_weather_info(latitude, longitude, searched_date):
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&hourly=rain&daily=rain_sum&timezone=Europe%2FLondon&start_date={searched_date}&end_date={searched_date}"
+    response = requests.get(url=url)
+    print(response.status_code)
+    if response.status_code == 200:
+        print(response.json())
+        return response.json()
+    else:
+        print("You have put the wrong data")
+        return None
+
+response = get_weather_info(latitude, longitude, searched_date)
+weather_result = response.get("daily").get("rain_sum")[0]
+# weather_info = "pada" if weather_result > 0 else "nie pada"
+
+if weather_result > 0:
+    weather_info = "pada"
+elif weather_result == 0:
+    weather_info = "nie pada"
+else:
+    weather_info = "no f*** idea"
+
+
+print(weather_result)
+file_handler.write_file(city, searched_date, weather_info)
+
+
+
+
+
+
---
+++ task_with_external_library/file_handler_rain.py
@@ -0,0 +1,24 @@
+import json
+
+class FileHandler:
+    def __init__(self, filename):
+        self.filename = filename
+        self.data = self.read_file()
+
+    def read_file(self):
+        with open(self.filename, 'r', encoding="utf-8") as file:
+            return json.loads(file.read())
+
+    def write_file(self, city, date, new_entry):
+        if not self.data.get(city):
+            self.data[city] = {}
+        self.data[city][date] = new_entry
+        with open(self.filename, 'w', encoding="utf-8") as file:
+            file.write(json.dumps(self.data, indent=4))
+
+    def check_if_date_exists(self, city, searched_date):
+        for searched_city in self.data:
+            if searched_city == city:
+                for date, info in self.data[searched_city].items():
+                    if date == searched_date:
+                        return info
